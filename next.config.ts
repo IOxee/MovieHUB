6059c94891b6328682c6343064b10d87b5092c12
FractalGameStudios <FractalGameStudios@users.noreply.github.com>
2026-01-09 16:46:40 +0100

    Try fix workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
+  output: 'export',
   turbopack: {
-    // Ensure Turbopack uses this project as the workspace root
-    root: __dirname,
+    root: __dirname
   },
+  images: {
+    unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/MovieHub' : '',
+  },
+  basePath: isProd ? '/MovieHub' : '',
+  assetPrefix: isProd ? '/MovieHub/' : ''
 };
 
 export default nextConfig;
