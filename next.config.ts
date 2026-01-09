45413911e27dee550946b93f6bbf8c15242ddec6
FractalGameStudios <FractalGameStudios@users.noreply.github.com>
2026-01-09 16:50:27 +0100

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production'
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  output: 'export',
-  turbopack: {
-    root: __dirname
-  },
-  images: {
-    unoptimized: true
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? '/MovieHub' : '',
-  },
-  basePath: isProd ? '/MovieHub' : '',
-  assetPrefix: isProd ? '/MovieHub/' : ''
+  output: "export",
+  images: { unoptimized: true },
+  env: { NEXT_PUBLIC_BASE_PATH: isProd ? "/MovieHub" : "" },
+  basePath: isProd ? "/MovieHub" : "",
+  assetPrefix: isProd ? "/MovieHub/" : ""
 };
 
 export default nextConfig;
